82cee173ad02238759b1cdb1aaba9e341989c584
Andrew Arnott <andrewarnott@gmail.com>
2018-04-12 14:32:23 -0700

    Add project files.

---
+++ StreamJsonRpc.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamJsonRpc.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
